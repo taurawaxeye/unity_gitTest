376f738e2a2e4aa51062ce1e47f99736f66994d8
Taura J Greig <taurajgreig@outlook.com>
2021-10-27 13:18:34 +1300

    init commit

---
+++ Assets/AutoRotate.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotate : MonoBehaviour
+{
+    [SerializeField] private Vector3 rotationAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate( rotationAmount * Time.deltaTime );
+
+        // random test
+        // random test 2
+        // random test 3 
+        // random test 4
+        // random test 5
+        // random test 6
+        // random test 7
+        // random test 8
+        // random test 9
+        // random test 10
+        // random test 11
+        // random test 12
+        // random test 13
+        // random test 14
+        // random test 15
+        // random test 16
+        // random test 17
+        // random test 18
+        // random test 19
+        // random test 20
+        // random test 21
+        // random test 22
+        // random test 23
+        // random test 24
+        // random test 25
+        // random test 26
+        // random test 27
+        // random test 28
+        // random test 29
+        // random test 30
+        // random test 31
+        // random test 32
+        // random test 33
+        // random test 34
+        // random test 35
+        // random test 36
+        // random test 37
+        // random test 38
+        // random test 39
+        // random test 40
+        // random test 41
+        // random test 42
+        // random test 43
+        // random test 44
+        // random test 45
+        // random test 46
+        // random test 47
+        // random test 48
+        // random test 49
+        // random test 50
+        // random test 51
+        // random test 52
+        // random test 53
+        // random test 54
+        // random test 55
+    }
+}
---
+++ Assets/Editor/BuildScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+
+namespace ProjectBuild
+{
+    public class BuildScript
+    {
+        [MenuItem("Build/WEB GL Build")]
+        public static void MyBuild()
+        {
+            Debug.Log( "Starting Build . . ." );
+
+            // var editorBuildSettings = PlayerSettings.settings
+            // var buildPlayerOptions = new BuildPlayerOptions();
+            // BuildPlayerWindow.DefaultBuildMethods.GetBuildPlayerOptions(buildPlayerOptions);
+
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+            var scenes = new List<string>();
+            foreach ( var scene in EditorBuildSettings.scenes )
+            {
+                Debug.Log(scene.path);
+                scenes.Add( $"{ scene.path }" );
+            }
+            buildPlayerOptions.scenes = scenes.ToArray();
+            buildPlayerOptions.locationPathName = $"{Application.dataPath.Replace("Assets", "")}builds/WebGl";
+            buildPlayerOptions.target = BuildTarget.WebGL;
+            buildPlayerOptions.options = BuildOptions.None;
+
+            Debug.Log( "Build Options creating . . . " );
+            Debug.Log( $"Saving buidl to {buildPlayerOptions.locationPathName}" );
+
+            BuildReport report = BuildPipeline.BuildPlayer( buildPlayerOptions );
+            BuildSummary summary = report.summary;
+
+            if (summary.result == BuildResult.Succeeded)
+            {
+                Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+            }
+
+            if (summary.result == BuildResult.Failed)
+            {
+                Debug.Log("Build failed");
+            }
+        }
+    }
+}
